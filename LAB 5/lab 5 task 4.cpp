a4e4cd3a83a085063c90aca30344486da65d5e52
Hanya321 <k250523@nu.edu.pk>
2025-09-27 02:01:26 -0700

    Add files via upload

---
+++ LAB 5/lab 5 task 4.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{int a,b;
+printf("Enter first number:");
+scanf("%d",&a);
+printf("Enter second number:");
+scanf("%d",&b);
+a>b?printf("%d is greatest",a):printf("%d is greatest",b);
+return 0;}
+
