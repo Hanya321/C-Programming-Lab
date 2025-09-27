2dea925d6738bef7e4ce8e6b13d2f52089c77ebc
Hanya321 <k250523@nu.edu.pk>
2025-09-27 02:00:20 -0700

    Add files via upload

---
+++ LAB 5/lab 5 task 1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{int a,b;
+printf("Enter first number:");
+scanf("%d",&a);
+printf("Enter second number:");
+scanf("%d",&b);
+printf("The result of a-b=%d",a-b);
+printf("\nThe result of (a-b)/2=%d",(a-b)/2);
+printf("\nThe result of (a-b)/2*b=%d",(a-b)/2*b);
+printf("\nFinal answer of (a-b)/2*b+a)=%d",(a-b)/2*b+a);
+
+return 0;}
+
