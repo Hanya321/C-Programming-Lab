086eda656e245da8b67740342fab2b0f27ac8f5f
Hanya321 <k250523@nu.edu.pk>
2025-10-09 09:31:29 -0700

    Add files via upload

---
+++ LAB 6/Lab 6 task 5.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{int fact=1,num;
+printf("Enter a number:");
+scanf("%d",&num);
+for(int i=1;i<=num;i++)
+{fact=fact*i;
+}
+printf("Factorial of the number:%d",fact);
+}
